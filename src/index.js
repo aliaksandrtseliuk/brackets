445ae4772d4d2a221e70c721f0abbe9ec7f0be93
aliaksandrtseliuk <52308758+aliaksandrtseliuk@users.noreply.github.com>
2020-03-07 16:03:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let avString = str
+  let newBracketsConfig = []
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    newBracketsConfig.push(bracketsConfig[i][0] + bracketsConfig[i][1])
+  }
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < newBracketsConfig.length; j++) {
+      avString = avString.replace(newBracketsConfig[j], '')
+    }
+    if (avString.length == 0) {
+      return true
+    }
+  }
+  return false
 }
